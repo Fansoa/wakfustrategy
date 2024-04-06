a9111aa6acaba87800b5355d8e9c320c7b72244b
fansoa <fverin.contactpro@gmail.com>
2024-04-06 23:06:29 +0200

    🏷️: update types

--- components/Menu/components/ButtonMenu/index.js
+++ components/Menu/components/ButtonMenu/index.js
@@ -9,25 +9,25 @@ const ButtonMenu = ({href, imgSrc, label, style}) => {
   const [isHover, setIsHover] = useState(false)
 
   return (
-        <StyledLink href={`/encyclopedia/${href}`} onMouseOver={()=> setIsHover(true)} onMouseOut={()=> setIsHover(false)}>
-          <ButtonMenuContainer $isHover={isHover}>
-            <Image
-              src={imgSrc}
-              alt={`${label} illustration`}
-              width={100}
-              height={100}
-              priority={false}
-              style={style}
-            />
-            <Label>{label}</Label>
-          </ButtonMenuContainer>
-        </StyledLink>
+    <StyledLink href={`/encyclopedia/${href}`} onMouseOver={()=> setIsHover(true)} onMouseOut={()=> setIsHover(false)}>
+      <ButtonMenuContainer $isHover={isHover}>
+        <Image
+          src={imgSrc}
+          alt={`${label} illustration`}
+          width={100}
+          height={100}
+          priority={false}
+          style={style}
+        />
+        <Label>{label}</Label>
+      </ButtonMenuContainer>
+    </StyledLink>
   )
 }
 
 ButtonMenu.propTypes = {
   href: PropTypes.string,
-  imgSrc: PropTypes.string,
+  imgSrc: PropTypes.oneOfType([PropTypes.string, PropTypes.shape({})]),
   label: PropTypes.string,
   style: PropTypes.shape({})
 }
